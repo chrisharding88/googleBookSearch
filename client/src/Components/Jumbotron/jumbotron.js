c06cb22c8e074a9a000569bf8f8df1293a9859b6
Christopher Harding <chrisharding88@gmail.com>
2019-12-26 13:17:56 -0500

    settiing up the jumbotron

---
+++ client/src/Components/Jumbotron/jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css"
+
+function jumbotron(){
+    return (
+        <div className="jumbotron">
+            <img className="logo" src="image/logo.jpg"></img><h1 className = "title">Book Search</h1>
+        </div>
+    )
+}
+
+export default jumbotron;
